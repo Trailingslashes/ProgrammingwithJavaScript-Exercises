339cce046fe58178bdeeb336b264e328309010bc
Cole Phillips <colephillips1@gmail.com>
2023-06-06 10:52:20 -0400

    Add solutions - loops & iterating chapter

---
+++ loops_and_iterating/age.js
@@ -0,0 +1,5 @@
+let age = 20
+console.log(`You are ${age} years old.`)
+for (let a = 10; a <= 40; a+= 10) {	
+	console.log(`In ${a} years you will be ${age + a}.`)
+}
---
+++ loops_and_iterating/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let tries = 0;
+// let result = randomNumberBetween(1, 6);
+// tries += 1;
